6dc733e80ace805e6423a15506df8a0cce8d9aa0
Krit Bannachaisirisuk <gmkipladen@gmail.com>
2021-09-22 00:15:17 +0700

    Add different state to main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
-import { FunctionComponent } from 'react';
+import { FunctionComponent, useEffect } from 'react';
 import { useAppDispatch, useAppSelector } from './app/hook';
 import { NumPad } from './components/NumPad/NumPad';
 import { Page } from './features/pageConfig/types';
 import './App.scss';
 import { authPin } from './features/authentication/authenticationSlice';
+import { changePage } from './features/pageConfig/pageConfigSlice';
+import { AuthStatus } from './features/authentication/types';
 
 const App: FunctionComponent = () => {
     const dispatch = useAppDispatch();
     const currentPage = useAppSelector((state) => state.pageConfig.currentPage);
+    const authStatus = useAppSelector((state) => state.auth.status);
     const currentBalance = useAppSelector((state) => state.auth.currentBalance);
+
+    useEffect(() => {
+        if (authStatus === AuthStatus.Success) {
+            dispatch(changePage(Page.WITHDRAW));
+        }
+        if (authStatus === AuthStatus.Error) {
+            dispatch(changePage(Page.ERROR));
+        }
+    }, [authStatus]);
 
     const onAuthClick = async (pin?: string) => {
         dispatch(authPin({ pin: pin || '' }));
@@ -19,6 +31,8 @@ const App: FunctionComponent = () => {
             <p>{currentBalance}</p>
             <header className="App-header">
                 {currentPage === Page.LOGIN && <NumPad pinLength={4} onAuthClick={onAuthClick} />}
+                {currentPage === Page.WITHDRAW && <h1>Withdraw</h1>}
+                {currentPage === Page.ERROR && <h1>ERROR!</h1>}
             </header>
         </div>
     );
