7230e965138f448ea22dd71d2844f4e5f0a677f1
Krit Bannachaisirisuk <gmkipladen@gmail.com>
2021-09-23 11:30:10 +0700

    Fix unit test failed

--- src/services/Dispenser/DispenserConfigurationBuilder.test.ts
+++ src/services/Dispenser/DispenserConfigurationBuilder.test.ts
@@ -1,14 +1,15 @@
 import 'jest';
+import { generateItem } from 'utils/mockUtils';
 import { DispenserConfigurationBuilder } from './DispenserConfigurationBuilder';
 
 describe('DispenserConfigurationBuilder', () => {
     it('should return configuration correctly', () => {
         const config = new DispenserConfigurationBuilder().withNotes(20, 3).withNotes(10, 1).build();
         expect(config.totalNotes).toEqual([
-            { name: '20', value: 20 },
-            { name: '20', value: 20 },
-            { name: '20', value: 20 },
-            { name: '10', value: 10 },
+            generateItem(20, 1),
+            generateItem(20, 2),
+            generateItem(20, 3),
+            generateItem(10, 1),
         ]);
     });
 });
